fb80bb29da5545639bb1cde296f8c451804aa70f
Sonia Boller <sonia.boller@NY-005744-AP.frogdesign.com>
2020-02-26 16:49:47 -0500

    pdf link, mailchimp working, css tweaks

--- scripts.js
+++ scripts.js
@@ -14,9 +14,10 @@
     })
 
     requestButton.on('click', () => {
-        if (screen.width >= 768 && !slickActive) {
+        if (window.innerWidth >= 768 && !slickActive) {
             // if not mobile and slick hasn't been initiated 
             // necessary to not use openModal() here because slick needs to know size of request-content div before running, which cannot be done if display: none
+            $('#mc-embedded-subscribe').addClass('button__disabled');
             requestContent.css('display', 'flex');
             modal.css('display', 'block');
             $('.request-info').slick({
@@ -25,24 +26,29 @@
             });
             $('.slick-next').on('click', (e) => {
                 if ($(e.target).hasClass('slick-disabled')) {
-                    $('#submit-button').removeClass('button__disabled');
+                    $('#mc-embedded-subscribe').removeClass('button__disabled');
                 }
             })
             $('.slick-prev').on('click', (e) => {
-                $('#submit-button').addClass('button__disabled');
+                $('#mc-embedded-subscribe').addClass('button__disabled');
             })
             slickActive = true;
             setTimeout(function() {
                 modal.css('display', 'block');
             }, 100);
             
-        } else if (screen.width >= 768 && slickActive) {
+        } else if (window.innerWidth >= 768 && slickActive) {
             // if not mobile and slick has been initiated
             openModal();
-        } else {
+        } else if (window.innerWidth <= 768 && !slickActive){
+        	$('#mc-embedded-subscribe').removeClass('button__disabled');
+            slickActive = false;
+            openModal();
+        }
+        else{
             // if mobile
             $('.request-info').slick('unslick');
-            $('#submit-button').removeClass('button__disabled');
+            $('#mc-embedded-subscribe').removeClass('button__disabled');
             slickActive = false;
             openModal();
         }
